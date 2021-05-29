fb92b0bf4e9985a1c3ffc8c05eb2311bcb85854b
hengsoo <hengsoo.sjh@gmail.com>
2021-05-29 22:53:52 +0800

    feat: expandable comment

--- app/src/main/java/com/example/shiristory/ui/timeline/PostAdapter.kt
+++ app/src/main/java/com/example/shiristory/ui/timeline/PostAdapter.kt
@@ -1,18 +1,28 @@
 package com.example.shiristory.ui.timeline
 
+import android.app.Activity
+import android.content.Context
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
+import android.widget.ListView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
-import com.bumptech.glide.annotation.GlideModule
 import com.example.shiristory.R
 import com.example.shiristory.service.timeline.models.Post
+import com.like.LikeButton
+import com.like.OnLikeListener
 import de.hdodenhof.circleimageview.CircleImageView
+import org.w3c.dom.Text
+
 
 class PostAdapter(private val dataSet: List<Post>) :
     RecyclerView.Adapter<PostAdapter.PostViewHolder>() {
+
+    private val TAG = this.javaClass.name
 
     /**
      * Provide a reference to the type of views that you are using
@@ -23,8 +33,11 @@ class PostAdapter(private val dataSet: List<Post>) :
         val postAuthor: TextView = view.findViewById(R.id.post_author)
         val postAuthorPic: CircleImageView = view.findViewById(R.id.post_author_pic)
         val postCreatedAt: TextView = view.findViewById(R.id.post_created_at)
+        val postLikeButton: LikeButton = view.findViewById(R.id.post_like_button)
         val postLikeCount: TextView = view.findViewById(R.id.post_like_count)
+        val postCommentButton: LikeButton = view.findViewById(R.id.post_comment_button)
         val postCommentCount: TextView = view.findViewById(R.id.post_comment_count)
+        val postCommentLayout: LinearLayout = view.findViewById(R.id.post_comments_layout)
 
         init {
             // Define click listener for the ViewHolder's View.
@@ -36,6 +49,9 @@ class PostAdapter(private val dataSet: List<Post>) :
         // Create a new view, which defines the UI of the list item
         val view = LayoutInflater.from(viewGroup.context)
             .inflate(R.layout.post_item_plain_text, viewGroup, false)
+
+        val commentView = LayoutInflater.from(viewGroup.context)
+            .inflate(R.layout.post_item_comment, viewGroup, false)
 
         return PostViewHolder(view)
     }
@@ -50,6 +66,45 @@ class PostAdapter(private val dataSet: List<Post>) :
         viewHolder.postCreatedAt.text = post.createdAt
         viewHolder.postLikeCount.text = post.likes.size.toString()
         viewHolder.postCommentCount.text = post.comments.size.toString()
+
+        viewHolder.postCommentLayout.removeAllViews()
+
+        for (comment in post.comments) {
+            val commentView = LayoutInflater.from(viewHolder.postCommentLayout.context)
+                .inflate(R.layout.post_item_comment, viewHolder.postCommentLayout, false)
+
+            commentView.findViewById<TextView>(R.id.post_comment_author).text =
+                comment.author.username
+            commentView.findViewById<TextView>(R.id.post_comment_created_at).text =
+                comment.createdAt
+            commentView.findViewById<TextView>(R.id.post_comment_comment).text = comment.comment
+
+            viewHolder.postCommentLayout.addView(commentView)
+        }
+
+        viewHolder.postLikeButton.setOnLikeListener(
+            object : OnLikeListener {
+                override fun liked(likeButton: LikeButton) {
+                    Log.d(TAG, "like " + post.content)
+                }
+
+                override fun unLiked(likeButton: LikeButton) {
+                    Log.d(TAG, "unlike" + post.content)
+                }
+            }
+        )
+
+        viewHolder.postCommentButton.setOnLikeListener(
+            object : OnLikeListener {
+                override fun liked(likeButton: LikeButton) {
+                    viewHolder.postCommentLayout.visibility = View.VISIBLE
+                }
+
+                override fun unLiked(likeButton: LikeButton) {
+                    viewHolder.postCommentLayout.visibility = View.GONE
+                }
+            }
+        )
 
         Glide.with(viewHolder.itemView).load(post.author.profilePicUrl)
             .into(viewHolder.postAuthorPic);
