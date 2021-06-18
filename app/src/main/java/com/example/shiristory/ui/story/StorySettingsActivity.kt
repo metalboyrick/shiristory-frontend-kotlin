5862de1ba23bd62e606d6f250f131da7abf7c258
Rick <rsulisthio@gmail.com>
2021-06-18 10:57:41 +0700

    feat: add member list recyclerview in story settings page

--- app/src/main/java/com/example/shiristory/ui/story/StorySettingsActivity.kt
+++ app/src/main/java/com/example/shiristory/ui/story/StorySettingsActivity.kt
@@ -6,6 +6,8 @@ import android.widget.EditText
 import android.widget.Switch
 import androidx.activity.viewModels
 import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.shiristory.R
 import com.example.shiristory.service.common.Constants.FINISHED
 import com.example.shiristory.service.common.Constants.ONGOING
@@ -14,28 +16,40 @@ class StorySettingsActivity : AppCompatActivity() {
 
     private var _currentGroupId : String? = ""
     private val _model: StorySettingsViewModel by viewModels()
-    private lateinit var groupNameView: EditText
-    private lateinit var groupStatusView: Switch
+    private lateinit var _groupNameView: EditText
+    private lateinit var _groupStatusView: Switch
+    private val _context = this@StorySettingsActivity
+    private lateinit var _recyclerView: RecyclerView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_story_settings)
 
-        groupNameView = findViewById(R.id.edit_group_name)
-        groupStatusView = findViewById(R.id.group_status_switch)
+        _groupNameView = findViewById(R.id.edit_group_name)
+        _groupStatusView = findViewById(R.id.group_status_switch)
+        _recyclerView = findViewById(R.id.group_members_recyclerview)
 
         // get the group id from the previous page
         _currentGroupId = intent.getStringExtra("groupId")
+
+        // inject linear layout manager
+        _recyclerView.layoutManager = LinearLayoutManager(_context)
 
         // fill in the information
         if(_currentGroupId != null){
             _model.getGroupInfo(_currentGroupId!!).observe(this, Observer {
                 if (it != null) {
-                    groupNameView.setText(it.name)
+                    _groupNameView.setText(it.name)
                     if(it.status == FINISHED)
-                        groupStatusView.isChecked = true
+                        _groupStatusView.isChecked = true
+                    _recyclerView.adapter = MemberListAdapter(it.members, _model)
                 }
             })
         }
+
+
+
+
+
     }
 }
