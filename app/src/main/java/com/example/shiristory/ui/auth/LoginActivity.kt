fe92c98e54c0f7a943ccb3ba8d2e1f0059f2669a
ngjuping <ngjuping2018@163.com>
2021-06-19 14:23:53 +0800

    feat: implement login api call

--- app/src/main/java/com/example/shiristory/ui/auth/LoginActivity.kt
+++ app/src/main/java/com/example/shiristory/ui/auth/LoginActivity.kt
@@ -5,21 +5,44 @@ import android.content.SharedPreferences
 import android.os.Bundle
 import android.provider.ContactsContract.CommonDataKinds.Email
 import android.provider.ContactsContract.CommonDataKinds.Phone
+import android.util.Log
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Observer
+import com.bumptech.glide.Glide
 import com.dd.processbutton.iml.ActionProcessButton
 import com.example.shiristory.R
+import com.example.shiristory.service.authentication.AuthenticationApiService
+import com.example.shiristory.service.authentication.models.Token
+import com.example.shiristory.service.common.RetrofitBuilder
+import com.example.shiristory.service.common.models.APIError
+import com.example.shiristory.service.timeline.TimelineApiService
+import com.example.shiristory.service.timeline.models.Post
+import com.example.shiristory.service.user.UserApiService
+import com.example.shiristory.service.user.models.User
+import com.google.gson.Gson
 import com.rengwuxian.materialedittext.MaterialEditText
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 
 class LoginActivity : AppCompatActivity() {
     private var btnSignIn:ActionProcessButton? = null
     private var login_username: MaterialEditText? = null
     private var login_password: MaterialEditText? = null
+
+    // shared preference related
     var sharedpreferences: SharedPreferences? = null
     val shiristory_PREFERENCES = "shiristory"
     val pref_username_field = "username"
-    val pref_password_field = "username"
+    val pref_password_field = "password"
+
+    // API related
+    private var _loginResponse: MutableLiveData<Token> = MutableLiveData<Token>()
+    private val _service: AuthenticationApiService = RetrofitBuilder.authenticationApiService
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -36,7 +59,50 @@ class LoginActivity : AppCompatActivity() {
 
     }
 
+    fun _login(credentials: Map<String,String>):LiveData<Token> {
+
+        _loginResponse.value = null
+
+        val call: Call<Token> =
+            _service.login(json_body = Gson().toJson(credentials))
+
+        call.enqueue(object : Callback<Token> {
+
+            override fun onFailure(call: Call<Token>, t: Throwable) {
+                Log.e("login", t.message!!)
+            }
+
+            override fun onResponse(call: Call<Token>, response: Response<Token>) {
+                _loginResponse.value = response.body()
+            }
+
+        })
+
+        return _loginResponse
+    }
+
     fun login(view: View){
+
+        val username: String = login_username!!.getText().toString()
+        val password: String = login_password!!.getText().toString()
+
+        if (username.length == 0 || password.length == 0){
+            return
+        }
+
+        val credentials = mapOf("username" to username,"password" to password)
+
+        _login(credentials).observe(this, Observer {
+            if(it != null){
+                val editor = sharedpreferences!!.edit()
+
+                editor.putString(pref_username_field, username)
+                editor.putString(pref_password_field, password)
+                editor.apply()
+            }
+        })
+
+        btnSignIn!!.setProgress(1)
 
         // call login API, and use the following as callback
 //        val username: String = login_username!!.getText().toString()
