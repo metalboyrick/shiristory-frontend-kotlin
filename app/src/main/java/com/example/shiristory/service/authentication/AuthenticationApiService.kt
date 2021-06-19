a463f2c0714bc5aee12905f862ba9011bc7de169
hengsoo <hengsoo.sjh@gmail.com>
2021-06-19 13:32:48 +0800

    feat: add auth api

--- app/src/main/java/com/example/shiristory/service/authentication/AuthenticationApiService.kt
+++ app/src/main/java/com/example/shiristory/service/authentication/AuthenticationApiService.kt
@@ -1,32 +1,38 @@
 package com.example.shiristory.service.authentication
 
+import com.example.shiristory.service.authentication.models.Token
 import com.example.shiristory.service.common.Constants.TIMELINE_API_PREFIX
+import com.example.shiristory.service.common.Constants.USER_API_PREFIX
 import com.example.shiristory.service.common.models.GenericResponse
-import com.example.shiristory.service.timeline.models.Comment
-import com.example.shiristory.service.timeline.models.Post
-import com.example.shiristory.service.timeline.models.PostsResponse
-import okhttp3.MultipartBody
-import okhttp3.RequestBody
 import retrofit2.Call
-import retrofit2.http.*
+import retrofit2.http.Body
+import retrofit2.http.Headers
+import retrofit2.http.POST
+import retrofit2.http.Path
 
 
 interface AuthenticationApiService {
-    
+
     @Headers("Content-Type: application/json")
-    @POST("${TIMELINE_API_PREFIX}/{post_id}/comment")
-    fun addComment(
-        @Path("post_id") post_id: String,
-        @Body  json_body: String
-    ): Call<Comment>
+    @POST("${USER_API_PREFIX}/jwt/refresh")
+    fun refresh(
+        @Body json_body: String
+    ): Call<Token>
 
-    @POST("${TIMELINE_API_PREFIX}/{post_id}/like")
-    fun likePost(
-        @Path("post_id") post_id: String
+    @Headers("Content-Type: application/json")
+    @POST("${USER_API_PREFIX}/signup")
+    fun signUp(
+        @Body json_body: String
+    ): Call<Token>
+
+    @Headers("Content-Type: application/json")
+    @POST("${USER_API_PREFIX}/login")
+    fun login(
+        @Body json_body: String
+    ): Call<Token>
+
+    @POST("${USER_API_PREFIX}/logout")
+    fun logout(
     ): Call<GenericResponse>
 
-    @POST("${TIMELINE_API_PREFIX}/{post_id}/dislike")
-    fun dislikePost(
-        @Path("post_id") post_id: String
-    ): Call<GenericResponse>
 }
